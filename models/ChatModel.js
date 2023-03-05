1365880fff86139f849c836bcf38da5f51272bcd
Shiriru <osu.skwize@yandex.ru>
2023-03-05 21:12:21 +0300

    modified:   models/ChatModel.js

--- models/ChatModel.js
+++ models/ChatModel.js
@@ -1,5 +1,7 @@
 const {Schema, model} = require("mongoose")
 
-const ChatSchema = new Schema()
+const ChatSchema = new Schema({
+    chatTitle: String,
+})
 
 module.exports = model("Chats", ChatSchema)
